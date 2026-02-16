df3c946859128b30a2cf94b4d05539d25133ad7d
JiraKorn404 <korn7716@gmail.com>
2026-02-16 14:07:38 +0700

    Update load_file.sql

--- scripts/load_file.sql
+++ scripts/load_file.sql
@@ -1,6 +1,6 @@
 -- Load Local File
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_order_payments_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_order_payments_dataset.csv'
 INTO TABLE bronze.olist_order_payments
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -19,7 +19,7 @@ SET
     payment_installment = NULLIF(@v_payment_installment, ''),
     payment_value = NULLIF(@v_payment_value, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_order_reviews_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_order_reviews_dataset.csv'
 INTO TABLE bronze.olist_order_reviews
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -41,7 +41,7 @@ SET
     review_creation_date = NULLIF(@v_review_creation_date, ''),
     review_answer_timestamp = NULLIF(@v_review_answer_timestamp, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_orders_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_orders_dataset.csv'
 INTO TABLE bronze.olist_orders
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -65,7 +65,7 @@ SET
     order_delivered_customer_date = NULLIF(@v_order_delivered_customer_date, ''),
     order_estimated_delivery_date = NULLIF(@v_order_estimated_delivery_date, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_products_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_products_dataset.csv'
 INTO TABLE bronze.olist_products
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -79,7 +79,7 @@ IGNORE 1 ROWS
     @v_product_photos_qty,
     @v_product_weight_g,
     @v_product_length_cm,
-    @v_product_heigth_cm,
+    @v_product_height_cm,
     @v_product_width_cm
 )
 SET
@@ -89,10 +89,10 @@ SET
     product_photos_qty = NULLIF(@v_product_photos_qty, ''),
     product_weight_g = NULLIF(@v_product_weight_g, ''),
     product_length_cm = NULLIF(@v_product_length_cm, ''),
-    product_heigth_cm = NULLIF(@v_product_heigth_cm, ''),
+    product_height_cm = NULLIF(@v_product_height_cm, ''),
     product_width_cm = NULLIF(@v_product_width_cm, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_sellers_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_sellers_dataset.csv'
 INTO TABLE bronze.olist_sellers
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -109,7 +109,7 @@ SET
     seller_city = NULLIF(@v_seller_city, ''),
     seller_state = NULLIF(@v_seller_state, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/product_category_name_translation.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/product_category_name_translation.csv'
 INTO TABLE bronze.olist_product_category_name_translation
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -122,25 +122,25 @@ IGNORE 1 ROWS
 SET
 	product_category_name_english = NULLIF(@v_product_category_name_english, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_customers_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_customers_dataset.csv'
 INTO TABLE bronze.olist_customers
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(
-	customer_id,
-    customer_unique_id,
-    @_vcustomer_zip_code_prefix,
-    @_vcustomer_city,
-    @_vcustomer_state
-)
-SET
-    customer_zip_code_prefix = NULLIF(@v_customer_zip_code_prefix, ''),
-    customer_city = NULLIF(@v_customer_city, ''),
-    customer_state = NULLIF(@v_customer_state, '');
+IGNORE 1 ROWS;
+-- (
+-- 	customer_id,
+--     customer_unique_id,
+--     @v_customer_zip_code_prefix,
+--     @v_customer_city,
+--     @v_customer_state
+-- )
+-- SET
+--     customer_zip_code_prefix = NULLIF(@v_customer_zip_code_prefix, ''),
+--     customer_city = NULLIF(@v_customer_city, ''),
+--     customer_state = NULLIF(@v_customer_state, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_geolocation_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_geolocation_dataset.csv'
 INTO TABLE bronze.olist_geolocation
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -159,7 +159,7 @@ SET
     geolocation_city = NULLIF(@v_geolocation_city, ''),
     geolocation_state = NULLIF(@v_geolocation_state, '');
 
-LOAD DATA LOCAL INFILE 'C:/Users/korn1/Downloads/ecomerce olist/olist_order_items_dataset.csv'
+LOAD DATA INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/Uploads/olist_order_items_dataset.csv'
 INTO TABLE bronze.olist_order_items
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
